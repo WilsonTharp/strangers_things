4be33354357894805c6c06fa39ac03a7734fb2ea
Bryson Davis <brysondavis65@gmail.com>
2021-08-10 16:50:22 -0500

    html changes

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div id="header">
+           <h1>Stranger Things</h1>
+           <div id="nav-bar">
+                <h3>Home</h3>
+                <h3>Posts</h3>
+                <h3>Profile</h3>
+                <h3>Log in</h3>
+            </div> 
+        </div>
+    )
+}
+
+export default Header;
