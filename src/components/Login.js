d685be165bf748cba2a86ba5152f3f28914470a4
Bryson Davis <brysondavis65@gmail.com>
2021-08-14 11:47:15 -0500

    change to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,10 +7,22 @@ const Login = (props) => {
         <div id="loginPage">
             <h1>Log In</h1>
             <div className="loginInputs">
-                <TextField id="outlined-basic-1" label="Username" variant="outlined" type="text" style= {{width: 350, marginBottom: 20, marginTop: 10}} />
+                <TextField id="outlined-basic-1"
+                           label="Username"
+                           name="username"
+                           value={user.username}
+                           variant="outlined"
+                           type="text"
+                           style= {{width: 350, marginBottom: 20, marginTop: 10}} />
             </div>
             <div className= "loginInputs">
-                <TextField id="outlined-basic=2" label="Password" variant="outlined" type= "password" style= {{width: 350, marginBottom: 20}} />
+                <TextField id="outlined-basic=2" 
+                           label="Password"
+                           name="password"
+                           value={user.password}
+                           variant="outlined"
+                           type= "password"
+                           style= {{width: 350, marginBottom: 20}} />
             </div>
             <button className= "loginButton">Log In</button>
             <a href="Some Link Goes Here">Don't have an account? Sign Up</a>
