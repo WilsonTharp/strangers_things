977ccb9bd5eddf4b409a5f7ec97a35cc84ddf25d
Wilson Tharp <wdtharp@ou.edu>
2021-08-16 20:15:40 -0500

    tokens can be found in local storage from login and signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,20 +3,39 @@ import { TextField } from "@material-ui/core";
 
 import { Link } from "react-router-dom";
 
-const Login = (props) => {
+const Login = ({logInRequest}) => {
     
     const [user, setUser] = useState({username: '', password: ''});
+
+    function handleInput(event) {
+        const userKey = event.target.attributes['name'].value;
+        const newState = {...user};
+        newState[userKey] = event.target.value;
+        setUser(newState);
+        console.log(user);
+    }
 
 
     function handleSubmit(event) {
         event.preventDefault();
+        logInRequest(user); 
+        // if (result.success===true){ //how to use authentication data here to route to different pages depending on result?
+        //     history.push('/home')
+        // } else {
+        //     return (
+        //         <h1>Log In attempt failed, please try again</h1>
+        //     )
+        // }
+
     }
     
     return (
         <div id="loginPage">
             <h1>Log In</h1>
+            <form onSubmit={handleSubmit}>
             <div className="loginInputs">
                 <TextField id="outlined-basic-1"
+                           onChange= {handleInput}
                            label="Username"
                            name="username"
                            value={user.username}
@@ -25,7 +44,8 @@ const Login = (props) => {
                            style= {{width: 350, marginBottom: 20, marginTop: 10}} />
             </div>
             <div className= "loginInputs">
-                <TextField id="outlined-basic=2" 
+                <TextField id="outlined-basic=2"
+                           onChange= {handleInput} 
                            label="Password"
                            name="password"
                            value={user.password}
@@ -34,6 +54,7 @@ const Login = (props) => {
                            style= {{width: 350, marginBottom: 20}} />
             </div>
             <button className= "loginButton">Log In</button>
+            </form>
             <Link to="/signup">Don't have an account? Sign Up</Link>
         </div>
     )
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
 
-const SignUp = (props) => {
+
+const SignUp = ({createNewUser}) => {
+
+    const [newUser, setNewUser] = useState({username: '', password: ''});
+    console.log(newUser);
+
+    function handleInput(event) {
+        const userKey = event.target.attributes['name'].value;
+        const newState = {...newUser};
+        newState[userKey] = event.target.value;
+        setNewUser(newState);
+        console.log(newUser);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        createNewUser(newUser);
+        // if(results.success==-true){ // HOW TO USE DATA FROM SIGN IN?
+        //     history.push('/home')
+        //     } else {
+        // return (
+        //     <div>
+        //        <h3> {result.data.message} </h3> //HOW TO SHOW THIS AFTER SUBMIT?
+        //     </div>
+        // )
+        //     }
+
+    }
+    
     return (
         <div id="signUp">
-        <h1>Sign Up</h1>
-        <input className= "loginInput" placeholder="Username"></input>
-        <input className= "loginInput" placeholder="Password"></input>
-        <button className= "loginButton">Sign Up</button>
+            <form onSubmit={handleSubmit}>
+                <h1>Sign Up</h1>
+                <input type="text" 
+                    onChange={handleInput}
+                    name="username" 
+                    value={newUser.username}
+                    className= "loginInput" 
+                    placeholder="Username"></input>
+                <input type="password" 
+                    onChange={handleInput} 
+                    name="password"
+                    value={newUser.password}
+                    className= "loginInput" 
+                    placeholder="Password"></input>
+                <button className= "loginButton">Sign Up</button>
+            </form>
         </div>
     )
 }
