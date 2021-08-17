3552aef621a44c63b75f76f4417c21188a576289
Bryson Davis <brysondavis65@gmail.com>
2021-08-16 20:28:14 -0500

    final monday merge???

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
-
-const Home = ({isLoggedin, setIsLoggedin}) => {
 import Login from "./Login"  //HOW TO ACCESS 'user' from Login component? {user.username}
 
 // need an if "logged in" render "Welcom...logged in as {user.username}"
 // else {data.message} these messages change depending on situation 'username already taken' for instance 'already logged in'
 
-const Home = (props) => {
+const Home = ({isLoggedin, setIsLoggedin}) => {
     return (
         <div id="homePage">
             <h1>Welcome to Stranger Things!</h1>
